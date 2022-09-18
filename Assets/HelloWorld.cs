4954c78521589b57be54026538793a3abffc7301
kiswuki <kovwiktoria@gmail.com>
2022-09-18 21:48:59 +0200

    09.18 gyakorlás
    változók

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 class HelloWorld : MonoBehaviour
 {
-    void Start()
+    void Start() //start metódus
     {
         Debug.Log("Hello World");
-        Debug.Log("Sziasztok");
-        Debug.Log($"My name is {name}");
+        Debug.Log("Sziasztok"); // ez meg egysoros komment, kód elé nem írható, csak utána, v önmagában, de akkoris csak egy sor lehet.
+        Debug.Log($"My name is {name}"); //kiadja a gameobject nevét (script)
     }
+
+    /* Több soros kommentet így kell írni
+     * Addig tart a komment,
+    Amíg be nem fejezem így. 
+    */
 }
